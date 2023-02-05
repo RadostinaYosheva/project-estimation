8e04328fc0499c14e0c2272bc5b49765c1713ed4
r.yosheva <radostina.yosheva@ocado.com>
2023-02-05 13:57:41 +0200

    Fix script for creating DBs

--- backend/src/db/createDBs.sql
+++ backend/src/db/createDBs.sql
@@ -1,27 +1,34 @@
 use estimation;
 
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE IF EXISTS Task;
+DROP TABLE IF EXISTS Project;
+DROP TABLE IF EXISTS User;
+
 -- TODO: Check if everything is ok (names, not null, default, types, foreign keys, etc.)
-CREATE TABLE Task (
-    id VARCHAR(36) PRIMARY KEY DEFAULT UUID(),
-    title VARCHAR(100) NOT NULL,
-    description VARCHAR(500),
-    type VARCHAR(20) NOT NULL,
-    FOREIGN KEY (assignee) REFERENCES User(id),
-    story_points INTEGER
-);
-
-CREATE TABLE Project (
-    id VARCHAR(36) PRIMARY KEY DEFAULT UUID(),
-    title VARCHAR(100) NOT NULL,
-    FOREIGN KEY (owner) REFERENCES User(id),
-    deadline DATETIME NOT NULL,
-    FOREIGN KEY (task_id) REFERENCES Task(id)
-);
-
 CREATE TABLE IF NOT EXISTS User (
-    id VARCHAR(36) PRIMARY KEY UUID(),
+    id VARCHAR(36) NOT NULL PRIMARY KEY DEFAULT UUID(),
     email VARCHAR(100),
     first_name VARCHAR(100) NOT NULL,
     second_name VARCHAR(100) NOT NULL
 );
 
+CREATE TABLE Task (
+    id VARCHAR(36) PRIMARY KEY DEFAULT UUID(),
+    title VARCHAR(100) NOT NULL,
+    assignee VARCHAR(200),
+    description VARCHAR(500),
+    type ENUM('epic', 'task', 'bug', 'discovery') NOT NULL,
+    story_points INTEGER,
+    FOREIGN KEY (assignee) REFERENCES User(id)
+);
+
+CREATE TABLE Project (
+    id VARCHAR(36) PRIMARY KEY DEFAULT UUID(),
+    title VARCHAR(100) NOT NULL,
+    owner VARCHAR(200),
+    deadline DATETIME NOT NULL,
+    task_id VARCHAR(36),
+    FOREIGN KEY (owner) REFERENCES User(id),
+    FOREIGN KEY (task_id) REFERENCES Task(id)
+);
