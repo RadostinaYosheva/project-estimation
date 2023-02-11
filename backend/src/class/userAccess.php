8fab0ec702e347e84afc96520e3112640066892c
r.yosheva <radostina.yosheva@ocado.com>
2023-02-11 17:11:04 +0200

    Add CRUD endpoints for user

---
+++ backend/src/class/userAccess.php
@@ -0,0 +1,127 @@
+<?php
+
+include_once '../class/user.php';
+
+class UserAccess {
+    private $conn;
+    private $db_table = "User";
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    public function getUsers() {
+        $sqlQuery = "SELECT id, first_name, last_name, password, email " .
+                    "FROM " . $this->db_table . "";
+        $statement = $this->conn->prepare($sqlQuery);
+        $statement->execute();
+
+        $users = array();
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
+            $user = User::loadFromJson((object) $row);
+
+            array_push($users, $user);
+        }
+        return $users;
+    }
+
+    public function getUser(string $userId) {
+        $sqlQuery = "SELECT id, first_name, last_name, password, email" .
+                    " FROM " . $this->db_table . 
+                    " WHERE id =  :userId";
+
+        $statement = $this->conn->prepare($sqlQuery);
+        $statement->bindParam(":userId", $userId);
+        $statement->execute();
+
+        $row = $statement->fetch(PDO::FETCH_ASSOC);
+        $user = User::loadFromJson((object) $row);
+        return $user;
+    }
+
+    public function getPasswordForUser(string $userId) {
+        $sqlQuery = "SELECT password" .
+                    " FROM " . $this->db_table . 
+                    " WHERE id =  :userId";
+
+        $statement = $this->conn->prepare($sqlQuery);
+        $statement->bindParam(":userId", $userId);
+        $statement->execute();
+
+        $row = $statement->fetch(PDO::FETCH_ASSOC);
+        $user = User::loadPassFromJson((object) $row);
+        return $user;
+    }
+
+    public function createUser(User $user) {
+        $sqlQuery = "INSERT INTO
+                        ". $this->db_table ."
+                    SET
+                        id = :id,
+                        first_name = :first_name, 
+                        last_name = :last_name, 
+                        password = :password,
+                        email = :email";
+
+        $email = $user->email ?? NULL;
+        
+        $statement = $this->conn->prepare($sqlQuery);
+        $statement->bindParam(":id", $user->id);
+        $statement->bindParam(":first_name", $user->first_name);
+        $statement->bindParam(":last_name", $user->last_name);
+        $statement->bindParam(":password", $user->password);
+        $statement->bindParam(":email", $email);
+    
+        $statement->execute();
+
+        return $this->getUser($user->id);
+    }
+
+    public function updateUser(User $user) {
+        $sqlQuery = "UPDATE " . $this->db_table . 
+        " SET
+            id = :id,
+            first_name = :first_name, 
+            last_name = :last_name, 
+            password = :password,
+            email = :email
+        WHERE id = :id";
+
+        $statement = $this->conn->prepare($sqlQuery);
+        $statement->bindParam(":id", $user->id);
+        $statement->bindParam(":first_name", $user->first_name);
+        $statement->bindParam(":last_name", $user->last_name);
+        $statement->bindParam(":password", $user->password);
+        $statement->bindParam(":email", $user->email);
+    
+        $statement->execute();
+
+        return $this->getUser($user->id);
+    }
+
+    public function deleteUser(string $userId) {
+        $sqlQuery = "DELETE FROM " . $this->db_table . " WHERE id = :user";
+
+        $statement = $this->conn->prepare($sqlQuery);
+        $statement->bindParam(":user", $userId);
+        $statement->execute();
+
+        if ($this->exists($userId) == true) {
+            throw new Exception ("Failed to delete user with id: " . $userId);
+        }
+
+        return "Successfully deleted user with id: " . $userId;
+    }
+
+    public function exists(string $userId) {
+        $sqlQuery = "SELECT * FROM ". $this->db_table . " WHERE id = :id";
+
+        $statement = $this->conn->prepare($sqlQuery);
+        $statement->bindParam(":id", $userId);
+        $statement->execute();
+
+        return $statement->rowCount() > 0;
+    }
+}
+
+?>
---
+++ backend/src/controllers/userControllers.php
@@ -0,0 +1,61 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    
+    include_once '../db/database.php';
+    include_once '../class/user.php';
+    include_once '../class/userAccess.php';
+    include_once '../utils/utils.php';
+
+    $database = new Database();
+    $db = $database->getConnection();
+    $userAccess = new UserAccess($db);
+
+    try {
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+            $uriSegments = Utils::getUriSegments();
+            $pathEnd = end($uriSegments);
+
+            if ($pathEnd == "userControllers.php") {
+                $users = $userAccess->getUsers();
+            } else if ($pathEnd == "password") {
+                $userId = $uriSegments[count($uriSegments)-2];
+                $users = $userAccess->getPasswordForUser($userId);
+            } else {
+                $users = $userAccess->getUser($pathEnd);
+            }
+
+            echo json_encode($users);
+        } 
+        else if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $body = json_decode(file_get_contents('php://input')); 
+            
+            $userRaw = User::loadFromJson($body);
+            $user = $userAccess->createUser($userRaw);
+
+            echo json_encode($user);
+        }
+        else if ($_SERVER["REQUEST_METHOD"] == "PUT") {
+            $body = json_decode(file_get_contents('php://input')); 
+            $id = Utils::loadIdFromJson($body);
+
+            $oldUser = $userAccess->getUser($id);
+            $newUser = User::loadPartialUserFromJson($id, $oldUser, $body);
+            $result = $userAccess->updateUser($newUser);
+
+            echo json_encode($result);
+        }
+        else if ($_SERVER["REQUEST_METHOD"] == "DELETE") {
+            $uriSegments = Utils::getUriSegments();
+            $userId = end($uriSegments);
+            $resultMessage = $userAccess->deleteUser($userId);
+
+            echo json_encode($resultMessage);
+        }
+    } catch (Exception $e) {
+        http_response_code(400);
+        $error = new stdClass();
+        $error->error = $e->getMessage();
+        echo json_encode($error);
+    }
+?>
